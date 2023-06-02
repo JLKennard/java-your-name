e1b1921ce9dbc6efb6c2d5ba0d0127e418008008
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-02 10:26:47 +0100

    chained trim method

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Name: ");
-            String name = scanner.nextLine();
+            String name = scanner.nextLine().trim();
             System.out.println("You are " + name);
         }
     }

4ca41266c87cfcd4defaba9fe5f8fcf45514d9a1
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-02 10:23:33 +0100

    scanner your name

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.println("You are " + name);
+        }
     }
 }
